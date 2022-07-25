94c19fa3c8ced786bf3face24a4d3e71703b668b
Samuele Girgenti <samuele.girgenti458@gmail.com>
2022-07-25 04:39:02 +0200

    update

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,7 +1,12 @@
 import React, { useEffect, useState } from "react";
+import { Button } from "@minteeble/ui-components";
 
 const Home = () => {
-  return <div className="home-page">Minteeble test 01</div>;
+  return (
+    <div className="home-page">
+      Minteeble test 01 <Button text="ddd" />
+    </div>
+  );
 };
 
 export default Home;
