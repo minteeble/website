8a6cd7347a392123dc5dbe076ac11a4a689ef34e
D0gg1 <chiaramotta06@gmail.com>
2023-02-12 18:10:10 +0100

    link target blank

--- src/components/ServicesCard/servicesCard.tsx
+++ src/components/ServicesCard/servicesCard.tsx
@@ -17,7 +17,7 @@ const ServicesCard = (props: servicesCardProps) => {
           <p className="servicesCard-content">{props.content}</p>
         </div>
         <div className="servicesCard-foot">
-          <a className="servicesCard-contact" href="https://wy7qp680aox.typeform.com/to/WpoqqxS5">Contact us <span className="servicesCard-arrow">→</span></a>
+          <a className="servicesCard-contact" target="_blank" href="https://wy7qp680aox.typeform.com/to/WpoqqxS5">Contact us <span className="servicesCard-arrow">→</span></a>
         </div>
       </div>
     </>
